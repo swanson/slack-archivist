3665d58ae14f8d42c6573a209defa993d45941f0
Matt Swanson <mdswanson@sep.com>
2019-02-22 14:14:03 -0500

    Update env vars

--- program.rb
+++ program.rb
@@ -7,11 +7,16 @@ Bundler.require(:default)
 require_relative "./archivist"
 
 Slack.configure do |config|
-  config.token = ENV['SLACK_BOT_TOKEN']
+  config.token = ENV['SLACK_TOKEN']
 end
 
 client = Slack::Web::Client.new
 
+dry_run = true
+if ENV['PRODUCTION'] == true
+  dry_run = false
+end
+
 Archivist.new(client, {
   username: ENV['BOT_USERNAME']
-}).run!
+}).run!(dry_run)
